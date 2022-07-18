be187b3684a5eb9a87931603e8c12ff0593733d5
Sambath Ranchaya <sambath.ranchaya@e-legalinc.com>
2022-07-18 10:56:52 -0500

    python oop first lesson

---
+++ oop1.py
@@ -0,0 +1,23 @@
+# position, name , age , level , salary
+se1 = ["Software Engineer", "Max", 20, "Junior", 5000]
+se2 = ["Software Engineer", "Lisa", 25, "Senior", 7000]
+
+# class
+class SoftwareEngineer:
+    # class attribute
+    alias = "Keyboard Magician"
+
+    def __init__(self, name, age , level , salary):
+        # instance attribute
+        self.name = name
+        self.age = age 
+        self.level = level
+        self.salary = salary
+
+# instance 
+se1 = SoftwareEngineer("Max", 20, "Junior", 5000)
+print(se1.name, se1.age)
+print(SoftwareEngineer.alias)
+se2 = SoftwareEngineer("Lisa", 25, "Senior", 7000)
+print(se2.name)
+
